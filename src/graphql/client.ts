b0bf551087cfe45aac0a83e1ffe5a49dde69dea5
Sergey <redmoloch@yandex.ru>
2024-10-28 09:39:53 +0300

    feat(books-gql-5): add default uri to apollo client

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: import.meta.env.VITE_GRAPHQL_URI,
+  uri: import.meta.env.VITE_GRAPHQL_URI || "http://localhost:3000/graphql",
   cache: new InMemoryCache(),
 });
